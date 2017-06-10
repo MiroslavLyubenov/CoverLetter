a0f9d68277ea738638e9b56f0a9c19fd604f903f
MiroslavLyubenov <m.liubenow@gmail.com>
2017-06-10 12:31:20 +0300

    Base setup

---
+++ README.md
@@ -0,0 +1,14 @@
+# Quick Start Static Site Builder
+### Pre-configured Gulp Build System
+<ul>
+	<li>jade</li>
+	<li>sass -> auto-prefixer</li>
+	<li>Source Maping sass</li>
+	<li>browser-sync</li>
+</ul>
+
+#Installation
+
+1. 'npm install'
+2. 'gulp'
+3. Rock out.
---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+"use strict";
+
+const gulp = require('gulp');
+const jade = require('gulp-jade');
+const prefix = require('gulp-autoprefixer');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync');
+
+const multipage = false;
+
+const paths = {
+    root: '',
+    sass: 'dev/sass',
+    jade: 'dev/jade',
+    css: 'css',
+    js: 'js'
+};
+
+function printError(error) {
+
+    // If you want details of the error in the console
+    console.log(error.toString());
+
+    this.emit('end')
+}
+
+gulp.task('browser-sync', ['sass', 'jade'], function () {
+    browserSync({
+        server: true,
+        notify: true,
+        browser: false
+    });
+});
+
+gulp.task('jade', function () {
+    var glob = (multipage) ? '/*.jade' : '/index.jade';
+
+    return gulp.src(paths.jade + glob)
+        .pipe(jade())
+        .on('error', printError)
+        .pipe(gulp.dest(paths.root));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(paths.sass + '/main.sass')
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            includePaths: [paths.sass],
+            outputStyle: 'nested'
+        }))
+        .on('error', printError)
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7']))
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest(paths.css))
+        .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('jade-rebuild', ['jade'], function () {
+    browserSync.reload();
+});
+
+gulp.task('browser-reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('watch', function () {
+    gulp.watch(paths.sass + '/*.sass', ['sass']);
+    gulp.watch(paths.jade + '/*.jade', ['jade-rebuild']);
+    gulp.watch(paths.js + '/*.js', ['browser-reload']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
