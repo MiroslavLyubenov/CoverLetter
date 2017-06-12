c570fcff5deb62257a1e486cc1e78c2af401850f
MiroslavLyubenov <m.liubenow@gmail.com>
2017-06-12 13:05:40 +0300

    side: Complete basic look

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-console.log('Write your Javascript here!')
+(function () {
+
+    // get data attr
+    var progressData = document.querySelectorAll('.progress__status'),
+        elemData;
+
+    progressData.forEach(function (elem) {
+        elemData = elem.getAttribute('data-level');
+        elem.style.minWidth = elemData;
+    });
+
+})();
