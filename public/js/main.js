86e9653e0687fb4ea22685a84a9c41265d32a71a
MiroslavLyubenov <m.liubenow@gmail.com>
2017-07-25 18:01:06 +0300

    Add shrink user section during scroll

--- public/js/main.js
+++ public/js/main.js
@@ -1,12 +1,60 @@
 (function () {
 
     // get data attr
-    var progressData = document.querySelectorAll('.progress__status'),
-        elemData;
+    var cssTransitionTime = 1000;
+    var scrollFlag = false;
+    var progressData = document.querySelectorAll('.progress__status');
+    var elemData;
+    var html = document.querySelector('html');
+    var container = document.querySelector('.container');
+    var sideHead = document.querySelector('.side__head');
 
+    function scroll(bool) {
+        if (bool === false)
+            html.style.overflow = 'hidden';
+        else
+            html.style.overflow = 'auto';
+    }
+
+    container.classList.remove('shrink__head');
+    console.log(container.classList.contains('shrink__head'));
+
+    // Add status to progress bars
     progressData.forEach(function (elem) {
         elemData = elem.getAttribute('data-level');
         elem.style.minWidth = elemData;
     });
+
+
+    // Presentation timeout
+    setTimeout(function () {
+        html.classList.remove('presentation', 'fadeIn');
+    }, 3000);
+
+
+    // Disable scrolling till head shrink is finished
+
+
+    // Listen for scroll
+    window.onscroll = function () {
+        var pagePosY = window.pageYOffset;
+
+        // shrink head
+        if (pagePosY > 30) {
+            container.classList.add('shrink__head');
+            // scroll(true);
+            if (!scrollFlag) {
+                scrollFlag = true;
+                scroll(false);
+                setTimeout(function () {
+                    scroll(true);
+                }, cssTransitionTime);
+            }
+
+        } else {
+            scrollFlag = false;
+            container.classList.remove('shrink__head')
+        }
+    }
 
 })();
